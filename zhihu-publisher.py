bb8cbf578e30e16f0ac7be653a28520ef60783a7
huangrt01 <huangrt01@163.com>
2020-09-16 15:20:09 +0800

    update file C++

--- zhihu-publisher.py
+++ zhihu-publisher.py
@@ -21,7 +21,7 @@ from shutil import copyfile
 ## Please change the GITHUB_REPO_PREFIX value according to your own GitHub user name and relative directory. ##
 ###############################################################################################################
 # Your image folder remote link
-GITHUB_REPO_PREFIX = "https://raw.githubusercontent.com/huangrt01/Markdown-Transformer-and-Uploader/master/Notes/"
+GITHUB_REPO_PREFIX = "https://raw.githubusercontent.com/huangrt01/Markdown-Transformer-and-Uploader/mynote/Notes/"
 COMPRESS_THRESHOLD = 5e5 # The threshold of compression
 
 # The main function for this program
